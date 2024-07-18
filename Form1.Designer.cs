a5e302fbc99cdc52213b339abbb0748e504379bf
Melike <Melike@MELIKEPC>
2024-07-18 23:30:21 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿
+namespace kendiuygform2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBoxad = new System.Windows.Forms.TextBox();
+            this.textBoxsoyad = new System.Windows.Forms.TextBox();
+            this.maskedTextBoxtc = new System.Windows.Forms.MaskedTextBox();
+            this.textBoxyaş = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBoxanaadı = new System.Windows.Forms.TextBox();
+            this.textBoxbabaadı = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBoxkurumadı = new System.Windows.Forms.TextBox();
+            this.maskedTextBoxtelno = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(359, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1010, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(561, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(204, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(305, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KİŞİ BİLGİLERİ:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(164, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(164, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soyad:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(158, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "T.C.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(158, 329);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Yaş:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(658, 166);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Anne Adı:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(658, 218);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Baba Adı:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(661, 278);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Öğrenim:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(661, 328);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(256, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Öğrenim Gördüğü Kurum Adı:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(661, 379);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(163, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Telefon Numarası:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(164, 374);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(128, 20);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Doğum Tarihi:";
+            // 
+            // textBoxad
+            // 
+            this.textBoxad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxad.Location = new System.Drawing.Point(316, 166);
+            this.textBoxad.Name = "textBoxad";
+            this.textBoxad.Size = new System.Drawing.Size(235, 27);
+            this.textBoxad.TabIndex = 11;
+            // 
+            // textBoxsoyad
+            // 
+            this.textBoxsoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxsoyad.Location = new System.Drawing.Point(316, 218);
+            this.textBoxsoyad.Name = "textBoxsoyad";
+            this.textBoxsoyad.Size = new System.Drawing.Size(235, 27);
+            this.textBoxsoyad.TabIndex = 12;
+            // 
+            // maskedTextBoxtc
+            // 
+            this.maskedTextBoxtc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskedTextBoxtc.Location = new System.Drawing.Point(316, 278);
+            this.maskedTextBoxtc.Mask = "00000000000";
+            this.maskedTextBoxtc.Name = "maskedTextBoxtc";
+            this.maskedTextBoxtc.Size = new System.Drawing.Size(235, 27);
+            this.maskedTextBoxtc.TabIndex = 13;
+            // 
+            // textBoxyaş
+            // 
+            this.textBoxyaş.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxyaş.Location = new System.Drawing.Point(316, 326);
+            this.textBoxyaş.Name = "textBoxyaş";
+            this.textBoxyaş.Size = new System.Drawing.Size(235, 27);
+            this.textBoxyaş.TabIndex = 14;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(316, 372);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(248, 27);
+            this.dateTimePicker1.TabIndex = 15;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // textBoxanaadı
+            // 
+            this.textBoxanaadı.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxanaadı.Location = new System.Drawing.Point(790, 163);
+            this.textBoxanaadı.Name = "textBoxanaadı";
+            this.textBoxanaadı.Size = new System.Drawing.Size(214, 27);
+            this.textBoxanaadı.TabIndex = 16;
+            // 
+            // textBoxbabaadı
+            // 
+            this.textBoxbabaadı.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxbabaadı.Location = new System.Drawing.Point(790, 215);
+            this.textBoxbabaadı.Name = "textBoxbabaadı";
+            this.textBoxbabaadı.Size = new System.Drawing.Size(214, 27);
+            this.textBoxbabaadı.TabIndex = 17;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Anaokulu",
+            "İlkokul",
+            "Ortaokul",
+            "Lise",
+            "Üniversite"});
+            this.comboBox1.Location = new System.Drawing.Point(790, 272);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(214, 28);
+            this.comboBox1.TabIndex = 18;
+            // 
+            // textBoxkurumadı
+            // 
+            this.textBoxkurumadı.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxkurumadı.Location = new System.Drawing.Point(944, 329);
+            this.textBoxkurumadı.Name = "textBoxkurumadı";
+            this.textBoxkurumadı.Size = new System.Drawing.Size(189, 27);
+            this.textBoxkurumadı.TabIndex = 19;
+            // 
+            // maskedTextBoxtelno
+            // 
+            this.maskedTextBoxtelno.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskedTextBoxtelno.Location = new System.Drawing.Point(827, 372);
+            this.maskedTextBoxtelno.Mask = "(999) 000-0000";
+            this.maskedTextBoxtelno.Name = "maskedTextBoxtelno";
+            this.maskedTextBoxtelno.Size = new System.Drawing.Size(177, 27);
+            this.maskedTextBoxtelno.TabIndex = 20;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(1161, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 95);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1761, 500);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBoxtelno);
+            this.Controls.Add(this.textBoxkurumadı);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBoxbabaadı);
+            this.Controls.Add(this.textBoxanaadı);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBoxyaş);
+            this.Controls.Add(this.maskedTextBoxtc);
+            this.Controls.Add(this.textBoxsoyad);
+            this.Controls.Add(this.textBoxad);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBoxad;
+        private System.Windows.Forms.TextBox textBoxsoyad;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxtc;
+        private System.Windows.Forms.TextBox textBoxyaş;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBoxanaadı;
+        private System.Windows.Forms.TextBox textBoxbabaadı;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBoxkurumadı;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxtelno;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kendiuygform2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ad:" + textBoxad.Text + " " + "Soyad: " + textBoxsoyad.Text + " " + "T.C." + maskedTextBoxtc.Text + " " + "yaş:" + textBoxyaş.Text + " " +
+                "Doğum Tarihi:" + dateTimePicker1.Text + " " + " Anne adı:" + textBoxanaadı.Text + " " + " Baba adı:" + textBoxbabaadı.Text + " " +
+                " Öğrenim:" + comboBox1.Text + " " + " Öğrenim gördüğü kurum adı:" + textBoxkurumadı.Text + " " + " Tel no:" + maskedTextBoxtelno.Text);
+
+        }
+    }
+}
